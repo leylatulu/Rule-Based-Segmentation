1943dbb6f60553f51fe92fb3d653f44639046a2d
Leyla TÜLÜ <98leylatulu@gmail.com>
2023-05-11 11:49:01 +0300

    deleted comment lines

--- rule_based_classification.py
+++ rule_based_classification.py
@@ -1,183 +1,72 @@
-
-#############################################
-# Kural Tabanlı Sınıflandırma ile Potansiyel Müşteri Getirisi Hesaplama
-#############################################
-
-#############################################
-# İş Problemi
-#############################################
-# Bir oyun şirketi müşterilerinin bazı özelliklerini kullanarak seviye tabanlı (level based) yeni müşteri tanımları (persona)
-# oluşturmak ve bu yeni müşteri tanımlarına göre segmentler oluşturup bu segmentlere göre yeni gelebilecek müşterilerin şirkete
-# ortalama ne kadar kazandırabileceğini tahmin etmek istemektedir.
-
-# Örneğin: Türkiye’den IOS kullanıcısı olan 25 yaşındaki bir erkek kullanıcının ortalama ne kadar kazandırabileceği belirlenmek isteniyor.
-
-
-#############################################
-# Veri Seti Hikayesi
-#############################################
-# Persona.csv veri seti uluslararası bir oyun şirketinin sattığı ürünlerin fiyatlarını ve bu ürünleri satın alan kullanıcıların bazı
-# demografik bilgilerini barındırmaktadır. Veri seti her satış işleminde oluşan kayıtlardan meydana gelmektedir. Bunun anlamı tablo
-# tekilleştirilmemiştir. Diğer bir ifade ile belirli demografik özelliklere sahip bir kullanıcı birden fazla alışveriş yapmış olabilir.
-
-# Price: Müşterinin harcama tutarı
-# Source: Müşterinin bağlandığı cihaz türü
-# Sex: Müşterinin cinsiyeti
-# Country: Müşterinin ülkesi
-# Age: Müşterinin yaşı
-
-################# Uygulama Öncesi #####################
-
-#    PRICE   SOURCE   SEX COUNTRY  AGE
-# 0     39  android  male     bra   17
-# 1     39  android  male     bra   17
-# 2     49  android  male     bra   17
-# 3     29  android  male     tur   17
-# 4     49  android  male     tur   17
-
-################# Uygulama Sonrası #####################
-
-#       customers_level_based        PRICE SEGMENT
-# 0   BRA_ANDROID_FEMALE_0_18  1139.800000       A
-# 1  BRA_ANDROID_FEMALE_19_23  1070.600000       A
-# 2  BRA_ANDROID_FEMALE_24_30   508.142857       A
-# 3  BRA_ANDROID_FEMALE_31_40   233.166667       C
-# 4  BRA_ANDROID_FEMALE_41_66   236.666667       C
-
-
-#############################################
-# PROJE GÖREVLERİ
-#############################################
-
-#############################################
-# GÖREV 1: Aşağıdaki soruları yanıtlayınız.
-#############################################
 import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 
-# Soru 1: persona.csv dosyasını okutunuz ve veri seti ile ilgili genel bilgileri gösteriniz.
-df = pd.read_csv('pythonProgramlama/Kural Tabanli Siniflandirma/persona.csv')
+df = pd.read_csv('persona.csv')
 
 def general_information(df):
-    print("##### INFO #####")
+    print("_____ INFO _____")
     print(df.info(), end='\n\n')
-    print("##### DESCRIPTION #####")
+    print("_____ DESCRIPTION _____")
     print(df.describe().T, end='\n\n')
-    print("##### SHAPE #####")
+    print("_____ SHAPE _____")
     print(df.shape,end ='\n\n')
-    print("##### MISSING #####")
+    print("_____ MISSING _____")
     print(df.isnull().sum().sort_values(ascending=False), end='\n\n')
-    print("##### QUANTILE #####")
+    print("_____ QUANTILE _____")
     print(df.quantile([0.01, 0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.99, 1]).T, end='\n\n')
-    print("##### UNIQUE #####")
+    print("_____ UNIQUE _____")
     print(df.nunique(), end='\n\n')
-    print("##### HEAD #####")
+    print("_____ HEAD _____")
     print(df.head(), end='\n\n')
-    print("##### TAIL #####")
+    print("_____ TAIL _____")
     print(df.tail(), end='\n\n')
-    print("##### SAMPLE #####")
+    print("_____ SAMPLE _____")
     print(df.sample(5), end='\n\n')
 
 general_information(df)
 
-# Soru 2: Kaç unique SOURCE vardır? Frekansları nedir?
 df.SOURCE.unique()
 df.SOURCE.value_counts()
 
-# Soru 3: Kaç unique PRICE vardır?
 df.PRICE.unique()
 
-# Soru 4: Hangi PRICE'dan kaçar tane satış gerçekleşmiş?
 df.PRICE.value_counts()
 
-# Soru 5: Hangi ülkeden kaçar tane satış olmuş?
 df.COUNTRY.value_counts()
 
-# Soru 6: Ülkelere göre satışlardan toplam ne kadar kazanılmış?
 df.groupby('COUNTRY')['PRICE'].sum()
 
-# Soru 7: SOURCE türlerine göre göre satış sayıları nedir?
 df.groupby('SOURCE')['PRICE'].count()
 
-# Soru 8: Ülkelere göre PRICE ortalamaları nedir?
 df.groupby('COUNTRY')['PRICE'].mean()
 
-# Soru 9: SOURCE'lara göre PRICE ortalamaları nedir?
 df.groupby('SOURCE')['PRICE'].mean()
 
-# Soru 10: COUNTRY-SOURCE kırılımında PRICE ortalamaları nedir?
 df.groupby(['COUNTRY', 'SOURCE'])['PRICE'].mean()
 
-#############################################
-# GÖREV 2: COUNTRY, SOURCE, SEX, AGE kırılımında ortalama kazançlar nedir?
-#############################################
 df.groupby(['COUNTRY', 'SOURCE', 'SEX', 'AGE'])['PRICE'].mean()
 
-#############################################
-# GÖREV 3: Çıktıyı PRICE'a göre sıralayınız.
-#############################################
-# Önceki sorudaki çıktıyı daha iyi görebilmek için sort_values metodunu azalan olacak şekilde PRICE'a uygulayınız.
-# Çıktıyı agg_df olarak kaydediniz.
 
 agg_df = df.groupby(['COUNTRY', 'SOURCE', 'SEX', 'AGE'])['PRICE'].mean().sort_values(ascending=False)
-
-
-#############################################
-# GÖREV 4: Indekste yer alan isimleri değişken ismine çeviriniz.
-#############################################
-# Üçüncü sorunun çıktısında yer alan PRICE dışındaki tüm değişkenler index isimleridir.
-# Bu isimleri değişken isimlerine çeviriniz.
-# İpucu: reset_index()
-# agg_df.reset_index(inplace=True)
-
 agg_df.index
 
 agg_df = agg_df.reset_index()
 
-#############################################
-# GÖREV 5: AGE değişkenini kategorik değişkene çeviriniz ve agg_df'e ekleyiniz.
-#############################################
-# Age sayısal değişkenini kategorik değişkene çeviriniz.
-# Aralıkları ikna edici olacağını düşündüğünüz şekilde oluşturunuz.
-# Örneğin: '0_18', '19_23', '24_30', '31_40', '41_70'
-
 agg_df['AGE'] = pd.cut(agg_df['AGE'], bins=[0, 18, 23, 30, 40, 70], labels=['0_18', '19_23', '24_30', '31_40', '41_70'])
-
-#############################################
-# GÖREV 6: Yeni level based müşterileri tanımlayınız ve veri setine değişken olarak ekleyiniz.
-#############################################
-# customers_level_based adında bir değişken tanımlayınız ve veri setine bu değişkeni ekleyiniz.
-# Dikkat!
-# list comp ile customers_level_based değerleri oluşturulduktan sonra bu değerlerin tekilleştirilmesi gerekmektedir.
-# Örneğin birden fazla şu ifadeden olabilir: USA_ANDROID_MALE_0_18
-# Bunları groupby'a alıp price ortalamalarını almak gerekmektedir.
-
 type(agg_df.AGE)
+
 agg_df["customers_level_based"] = (agg_df.COUNTRY + "_" + agg_df.SOURCE + "_" + agg_df.SEX + "_" + agg_df.AGE.astype("str")).apply(lambda x: x.upper())
 agg_df.customers_level_based.value_counts()
 agg_df = agg_df.groupby("customers_level_based").agg({"PRICE": "mean"}).reset_index()
 
-
-#############################################
-# GÖREV 7: Yeni müşterileri (USA_ANDROID_MALE_0_18) segmentlere ayırınız.
-#############################################
-# PRICE'a göre segmentlere ayırınız,
-# segmentleri "SEGMENT" isimlendirmesi ile agg_df'e ekleyiniz,
-# segmentleri betimleyiniz,
-
 agg_df["SEGMENT"] = pd.qcut(agg_df["PRICE"], 4, labels=["D", "C", "B", "A"])
 agg_df.groupby("SEGMENT").agg({"PRICE": ["mean", "max", "sum"]})
 
-#############################################
-# GÖREV 8: Yeni gelen müşterileri sınıflandırınız ne kadar gelir getirebileceğini tahmin ediniz.
-#############################################
-# 33 yaşında ANDROID kullanan bir Türk kadını hangi segmente aittir ve ortalama ne kadar gelir kazandırması beklenir?
+
 
 customer1 = "TUR_ANDROID_FEMALE_31_40"
 agg_df[agg_df.customers_level_based == customer1]
-
-# 35 yaşında IOS kullanan bir Fransız kadını hangi segmente ve ortalama ne kadar gelir kazandırması beklenir?
 
 customer2 = "FRA_IOS_FEMALE_31_40"
 agg_df[agg_df.customers_level_based == customer2]
